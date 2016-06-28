75ee21dd09dc0596a7d973765597ae533f4d40e0
victor <victor@vbonhomme.fr>
2016-06-28 10:36:38 +0800

    add template for marketData and instruments routes

--- src/api/instruments.js
+++ src/api/instruments.js
@@ -1,4 +1,12 @@
 const debug = require('debug')('api:instruments');
+import Boom from 'boom';
 import * as mongodb from '../mongodb';
 
-export async function getMain() { return 'getMain'; }
+export async function getMain() {
+  try {
+    Boom.notImplemented('method not implemented');
+  } catch (error) {
+    debug('getMain() Error: %o', error);
+    throw error;
+  }
+}
--- src/api/marketData.js
+++ src/api/marketData.js
@@ -1,6 +1,30 @@
 const debug = require('debug')('api:marketData');
+import Boom from 'boom';
 import * as mongodb from '../mongodb';
 
-export async function getCandleStick() { return 'getCandleStick'; }
-export async function getAvg() { return 'getAvg'; }
-export async function getAllMA() { return 'getAllMA'; }
+export async function getCandleStick() {
+  try {
+    Boom.notImplemented('method not implemented');
+  } catch (error) {
+    debug('getCandleStick() Error: %o', error);
+    throw error;
+  }
+}
+
+export async function getAvg() {
+  try {
+    Boom.notImplemented('method not implemented');
+  } catch (error) {
+    debug('getAvg() Error: %o', error);
+    throw error;
+  }
+}
+
+export async function getAllMA() {
+  try {
+    Boom.notImplemented('method not implemented');
+  } catch (error) {
+    debug('getAllMA() Error: %o', error);
+    throw error;
+  }
+}
