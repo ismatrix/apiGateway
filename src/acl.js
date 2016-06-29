1d78e757caf1b6d955438dca3cb7eaf317aca1ad
victor <victor@vbonhomme.fr>
2016-06-29 11:35:17 +0800

    add ACL, user rights, providing a koa middleware to auth: canKoa(action, ressource)

---
+++ src/acl.js
@@ -0,0 +1,71 @@
+const debug = require('debug')('acl');
+import Boom from 'boom';
+import NodeAclCb from 'acl';
+import Promise from 'bluebird';
+const NodeAcl = Promise.promisifyAll(NodeAclCb);
+const acl = new NodeAcl(new NodeAcl.memoryBackend());
+
+// Departments permissions
+acl.allow([
+  {
+    roles: ['系统部'], allows: [
+      { resources: 'reports/it', permissions: ['read', 'write', 'delete'] },
+      { resources: 'users', permissions: ['read', 'write', 'delete'] },
+    ] },
+  {
+    roles: ['市场部'], allows: [
+      { resources: 'reports/marketing', permissions: ['read', 'write', 'delete'] },
+    ] },
+  {
+    roles: ['交易部'], allows: [
+      { resources: 'reports/trading', permissions: ['read', 'write', 'delete'] },
+    ] },
+  {
+    roles: ['财务部'], allows: [
+      { resources: 'reports/finance', permissions: ['read', 'write', 'delete'] },
+    ] },
+  {
+    roles: ['总经办'], allows: [
+      { resources: 'reports/management', permissions: ['read', 'write', 'delete'] },
+    ] },
+  {
+    roles: ['客户'], allows: [
+      { resources: 'reports/customers', permissions: ['read'] },
+    ] },
+]);
+
+// Users permissions
+acl.allow([
+  {
+    roles: ['victor'], allows: [
+      { resources: 'funds', permissions: ['read', 'write', 'delete'] },
+      { resources: 'funds/:fundid', permissions: ['read', 'write', 'delete'] },
+    ] },
+]);
+
+
+// roles       {String|Array} Role(s) to check the permissions for.
+// permissions {String|Array} asked permissions.
+// resource    {String} resource to ask permissions for.
+async function can(roles, permissions, resource) {
+  try {
+    const isRoleAuthorized = await acl.areAnyRolesAllowed(roles, resource, permissions);
+    return isRoleAuthorized;
+  } catch (error) {
+    debug('canRole() Error: %o', error);
+  }
+}
+
+function canKoa(permissions, resource) {
+  return async (ctx, next) => {
+    const dpt = ctx.state.user.dpt;
+    const userid = ctx.state.user.userid;
+    const roles = dpt ? dpt.concat(userid) : [].concat(userid);
+    debug(roles);
+    const hasRight = await can(roles, permissions, resource);
+    if (!hasRight) throw Boom.forbidden('Access forbidden');
+    return next();
+  };
+}
+
+export { can, canKoa };
