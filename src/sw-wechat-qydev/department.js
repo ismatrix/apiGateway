4840937d0156fb02ccf12ca1bc1a537cb6e71f54
victor <victor@vbonhomme.fr>
2016-08-03 09:45:05 +0800

    fix getDepartmentById return value

--- src/sw-wechat-qydev/department.js
+++ src/sw-wechat-qydev/department.js
@@ -5,11 +5,11 @@ import Boom from 'boom';
 async function getDepartmentById(id) {
   try {
     const url = `${this.prefix}department/list?access_token=${this.accessToken}&id=${id}`;
-    const department = await fetch(url).then(res => res.json());
+    const departmentRes = await fetch(url).then(res => res.json());
 
-    if (department.errcode !== 0) throw Boom.notFound('qydev department api errcode not null');
+    if (departmentRes.errcode !== 0) throw Boom.notFound('qydev department api errcode not null');
 
-    return department;
+    return departmentRes.department;
   } catch (error) {
     debug(`getAccessToken() error: ${error}`);
     throw error;
