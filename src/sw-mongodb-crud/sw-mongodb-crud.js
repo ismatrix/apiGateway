b676716d9b9849b1c08ff9b8c8d6c1eff70d337c
tristan <wangwl1980@gmail.com>
2016-08-11 16:58:09 +0800

    add module daybar

--- src/sw-mongodb-crud/sw-mongodb-crud.js
+++ src/sw-mongodb-crud/sw-mongodb-crud.js
@@ -1,10 +1,11 @@
 import { mongoUrl } from './config';
 import * as mongodb from '../mongodb';
 
-const debug = require('debug')('sw-mongodb-crud');
+// const debug = require('debug')('sw-mongodb-crud');
 
 mongodb.connect(mongoUrl);
 
 export * as product from './product';
 export * as instrument from './instrument';
+export * as daybar from './daybar';
 export * as fund from './fund';
--- src/sw-mongodb-crud/test.js
+++ src/sw-mongodb-crud/test.js
@@ -1,6 +1,6 @@
 require('babel-register');
-// const product = require('./product');
-// product.runTest();
+
 const crud = require('./sw-mongodb-crud');
-crud.product.runTest();
-crud.instrument.runTest();
+// crud.product.runTest();
+// crud.instrument.runTest();
+crud.daybar.runTest();
