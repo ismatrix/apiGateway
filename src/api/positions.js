5cd6ef12efaedf82bd7d8d897ec78cca76969110
victor <victor@vbonhomme.fr>
2016-10-17 11:22:18 +0800

    require tradingday for getPositions

--- src/api/positions.js
+++ src/api/positions.js
@@ -7,8 +7,10 @@ const debug = createDebug('api:positions');
 export async function getPositions(fundid, tradingDay) {
   try {
     if (!fundid) throw Boom.badRequest('Missing fundid parameter');
+    if (!tradingDay) throw Boom.badRequest('Missing tradingDay parameter');
 
-    const dbPositions = await positionDB.getLast(fundid, tradingDay);
+    const dbPositions = await positionDB.get(fundid, tradingDay);
+    debug('dbPositions %o', dbPositions);
 
     if (dbPositions && dbPositions.positionslist && dbPositions.positionslist.position) {
       const positions = dbPositions.positionslist.position;
