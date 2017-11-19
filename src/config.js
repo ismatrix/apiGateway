b63a984094ed056ebbf4007c3132a020649ffa9f
tristan <wangwl1980@gmail.com>
2017-11-19 14:30:33 +0800

    debug work.weixin.auto

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ const productionConfig = {
     encodingAESKey: 'zC5bDgdQObhGEAOSQT2T0fzBQ6iCYgxObD5DgKH4GrS',
     token: 'GT5jrEUG8qZ4dtWrBieilrhjWVdIX9D',
     corpId: 'wwb684dd509851f6d3',
-    corpSecret: 'cWd1SUkX8hU-sLyuGovGwvFzHcVqpXfufpomDhOtc_5hcGVUKc6wJJTb4yo3k3tJ',
+    corpSecret: 'fosvKzBOZoQBV5ptke5azLdsuF2s1TXVCGjktKBh14E',
     authorizeCallbackURL: 'https://quantowin.com/api/public/weixin/qy/id=13/callback',
   },
   wechatGZHConfig: {
